f0e55fd42992d776a56aab8fa82a4e8acec1cfe3
pabalint <152804802+pabalint@users.noreply.github.com>
2025-05-02 12:23:22 +0200

    Termék nevek hibájának megoldás
    Mostmár helyesen jelennek meg az ObjectSearch tableből a terméknevek

---
+++ Dnn.Szinajanlo8/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
+
+namespace Szinajanlo8.Dnn.Dnn.Szinajanlo8.Models
+{
+    [TableName("hcc_Product")]
+    [PrimaryKey(nameof(SKU), AutoIncrement = false)]
+    public class Product
+    {
+        public int Id { get; set; }
+        public string SKU { get; set; }
+        public string bvin { get; set; }
+        public string ImageFileSmall { get; set; }
+        public string ImageFileMedium { get; set; }
+
+        public decimal ListPrice { get; set; }
+        public decimal SitePrice { get; set; }
+
+        public string TemplateName { get; set; }
+        public string RewriteUrl { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public long ProductTypeId { get; set; }
+    }
+}
